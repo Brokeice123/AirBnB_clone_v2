2627e3e32ef59c9c1b03cdf5fdbd0f7158267558
Brokeice123 <keithmarkomen@gmail.com>
2024-02-17 09:53:41 +0000

    Improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,3 +57,7 @@ class FileStorage:
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             FileStorage.__objects.pop(key, None)
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
